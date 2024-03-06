11f2583e05dcfe43b89acef3747033499298cc2f
dxquyet16 <quyetcoder16@gmail.com>
2024-03-06 17:26:24 +0700

    create upload dto

--- src/quan-ly-banner/dto/create-quan-ly-banner.dto.ts
+++ src/quan-ly-banner/dto/create-quan-ly-banner.dto.ts
@@ -1,1 +1,6 @@
-export class CreateQuanLyBannerDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateQuanLyBannerDto {
+    @ApiProperty({ description: "ma_phim", type: Number })
+    email: number;
+}
